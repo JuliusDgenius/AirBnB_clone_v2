d57a2aae50728352a9f2f77975e33b589fb8bf62
JuliusDgenius <ibejulius1@gmail.com>
2024-06-19 05:25:50 +0100

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application. """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello function return the string 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb function return the string 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """c function return the string 'C' followed by the value of text
+    - replace '_' with whitespace.
+
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """python function return the string 'Python' followed by the value of text
+    - replace '_' with whitespace.
+    """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
