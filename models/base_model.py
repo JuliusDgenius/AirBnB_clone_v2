aa49d97aa50c0d4512fc242d15437c3aef4ea75a
JuliusDgenius <ibejulius1@gmail.com>
2024-08-11 18:01:02 +0100

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,24 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import models
+from sqlalchemy import Integer, Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -29,16 +43,34 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dct = {}
+        dct.update(self.__dict__)
+        if "created_at" in dct:
+            dct["created_at"] = dct["created_at"].isoformat()
+        if "updated_at" in dct:
+            dct["updated_at"] = dct["updated_at"].isoformat()
+        if '_password' in dct:
+            dct['password'] = dct['_password']
+            dct.pop('_password', None)
+        if 'amenities' in dct:
+            dct.pop('amenities', None)
+        if 'reviews' in dct:
+            dct.pop('reviews', None)
+        dct["__class__"] = self.__class__.__name__
+        dct.pop('_sa_instance_state', None)
+        if not save_to_disk:
+            dct.pop('password', None)
+        return dct
+
+    def delete(self):
+        """
+        Public instance method that deletes an instance by
+        calling its instance method
+        """
+        storage.delete(self)
