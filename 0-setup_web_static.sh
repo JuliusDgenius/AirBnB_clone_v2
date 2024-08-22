e4efeed21da14d00b25388ef9a6c85f6a0f44372
JuliusDgenius <ibejulius1@gmail.com>
2024-08-22 20:22:24 +0100

    corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -34,4 +34,4 @@ printf %s "server {
         internal;
     }
 }" > /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,6 +19,7 @@ def do_deploy(archive_path):
     temp = archive_path.split('.')[0]
     file_name = temp.split('/')[1]
     dest = data_path + file_name
+    print(dest)
 
     try:
         put(archive_path, '/tmp')
@@ -28,7 +29,8 @@ def do_deploy(archive_path):
         run('sudo mv {}/web_static/* {}/'.format(dest, dest))
         run('sudo rm -rf {}/web_static/'.format(dest))
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(dest))
+        run('sudo ln -sfn {} /data/web_static/current'.format(dest))
         return True
-    except RuntimeError:
+    except RuntimeError as e:
+        print(f"Deployment failed {e}")
         return False
